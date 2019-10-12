f004e25a5cd7330b2b28b16df9fc7292478e3c3d
s3628730 <37060647+s3628730@users.noreply.github.com>
2019-10-12 15:23:50 +1100

    Update GameEngine.java
    adding astar and ghost movement

--- multiplayerPacMan/src/game/GameEngine.java
+++ multiplayerPacMan/src/game/GameEngine.java
@@ -1,108 +1,127 @@
 package game;
+
+import java.util.List;
+
+import aStarAlgorithm.AStar;
+import aStarAlgorithm.Node;
 import game.Enumeration.BlockType;
-import game.Enumeration.Direction;
 
 public class GameEngine {
 	private static GameMap map = new GameMap();
-	
+
 	public static void updateGame(GameData gameState) {
-		for(PlayerData player:gameState.getPlayers()) {
+
+		int minDistance = 1000;
+		List<Node> minPath = null;
+		for (PlayerData player : gameState.getPlayers()) {
 			movementUpdate(player);
+
+			List<Node> currentPath = ghostMovementUpdate(gameState.getGhost(), player);
+			if (currentPath.size() < minDistance) {
+				minDistance = currentPath.size();
+				minPath = currentPath;
+			}
+
 		}
+
+		minPath.get(0);
+
 		gameState.gameTick();
 	}
-	
+
 	private static void movementUpdate(PlayerData player) {
 		int x;
 		int y;
-		
-		switch(player.getBufferDirection()) {
-		case RIGHT:
-			x = player.getX() + 1;
-			y = player.getY();
-			if (x < map.getWidth() && 
-					map.getBlockAt(x,y) == BlockType.EMPTY_PATH)
-				player.useDirBuffer();
-			
-			break;
-			
-		case LEFT:
-			x = player.getX() - 1;
-			y = player.getY();
-			if (x >= 0 && 
-					map.getBlockAt(x,y) == BlockType.EMPTY_PATH)
-				player.useDirBuffer();
-			
-			break;
-			
-		case UP:
-			x = player.getX();
-			y = player.getY() - 1;
-			if (y >= 0 && 
-					map.getBlockAt(x,y) == BlockType.EMPTY_PATH)
-				player.useDirBuffer();
-			
-			break;
-		case DOWN:
-			x = player.getX();
-			y = player.getY() + 1;
-			if (y < map.getHeight() && 
-					map.getBlockAt(x,y) == BlockType.EMPTY_PATH)
-				player.useDirBuffer();
-			
-			break;
-			
-		default:
-			break;
-			
-		}
-		
-		switch(player.getDirection()) {
-		case RIGHT:
-			x = player.getX() + 1;
-			y = player.getY();
-			if (x < map.getWidth() && 
-					map.getBlockAt(x,y) == BlockType.EMPTY_PATH)
-				player.setPos(x,y);
-			
-			break;
-			
-		case LEFT:
-			x = player.getX() - 1;
-			y = player.getY();
-			if (x >= 0 && 
-					map.getBlockAt(x,y) == BlockType.EMPTY_PATH) 
-				player.setPos(x,y);
-			
-			break;
-			
-		case UP:
-			x = player.getX();
-			y = player.getY() - 1;
-			if (y >= 0 && 
-					map.getBlockAt(x,y) == BlockType.EMPTY_PATH) 
-				player.setPos(x,y);
-			
-			break;
-			
-		case DOWN:
-			x = player.getX();
-			y = player.getY() + 1;
-			if (y < map.getHeight() && 
-					map.getBlockAt(x,y) == BlockType.EMPTY_PATH)
-				player.setPos(x,y);
-			
-			break;
-			
-		default:
-			break;
-			
-		}
-		
-		
-		
-		
-		
-	}
-}
 
+		switch (player.getBufferDirection()) {
+		case RIGHT:
+			x = player.getX() + 1;
+			y = player.getY();
+			if (x < map.getWidth() && map.getBlockAt(x, y) == BlockType.EMPTY_PATH)
+				player.useDirBuffer();
+
+			break;
+
+		case LEFT:
+			x = player.getX() - 1;
+			y = player.getY();
+			if (x >= 0 && map.getBlockAt(x, y) == BlockType.EMPTY_PATH)
+				player.useDirBuffer();
+
+			break;
+
+		case UP:
+			x = player.getX();
+			y = player.getY() - 1;
+			if (y >= 0 && map.getBlockAt(x, y) == BlockType.EMPTY_PATH)
+				player.useDirBuffer();
+
+			break;
+		case DOWN:
+			x = player.getX();
+			y = player.getY() + 1;
+			if (y < map.getHeight() && map.getBlockAt(x, y) == BlockType.EMPTY_PATH)
+				player.useDirBuffer();
+
+			break;
+
+		default:
+			break;
+
+		}
+		switch (player.getDirection()) {
+		case RIGHT:
+			x = player.getX() + 1;
+			y = player.getY();
+			if (x < map.getWidth() && map.getBlockAt(x, y) == BlockType.EMPTY_PATH)
+				player.setPos(x, y);
+
+			break;
+
+		case LEFT:
+			x = player.getX() - 1;
+			y = player.getY();
+			if (x >= 0 && map.getBlockAt(x, y) == BlockType.EMPTY_PATH)
+				player.setPos(x, y);
+
+			break;
+
+		case UP:
+			x = player.getX();
+			y = player.getY() - 1;
+			if (y >= 0 && map.getBlockAt(x, y) == BlockType.EMPTY_PATH)
+				player.setPos(x, y);
+
+			break;
+
+		case DOWN:
+			x = player.getX();
+			y = player.getY() + 1;
+			if (y < map.getHeight() && map.getBlockAt(x, y) == BlockType.EMPTY_PATH)
+				player.setPos(x, y);
+
+			break;
+
+		default:
+			break;
+
+		}
+
+	}
+
+	private static List<Node> ghostMovementUpdate(GhostData ghost, PlayerData player) {
+		Node startNode = new Node(ghost.getX(), ghost.getY());
+		Node endNode = new Node(player.getX(), player.getY());
+		int rows = map.getHeight();
+		int cols = map.getWidth();
+
+		AStar aStar = new AStar(rows, cols, startNode, endNode);
+
+		// set walls
+		aStar.setBlocks(map.getWallCoords());
+
+		// find end Node
+		return aStar.findPath();
+	}
+
+}
