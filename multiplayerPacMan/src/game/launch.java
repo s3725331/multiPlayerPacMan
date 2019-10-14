53109a953d975ef5710f84ad3aad67c79d61dbba
s3722271 <37519572+s3722271@users.noreply.github.com>
2019-10-14 11:09:57 +1100

    Create launch.java
    Adding launch class

---
+++ multiplayerPacMan/src/game/launch.java
@@ -0,0 +1,90 @@
+package game;
+import java.util.Scanner;
+import java.util.regex.*;
+
+import gui.SimpleOutPut;
+import gui.TemplateFrame;
+import networking.Client;
+import networking.Server;
+
+public class launch {
+
+	public static void main(String[] args) {
+
+		Scanner player = new Scanner(System.in);
+		Scanner address = new Scanner(System.in);
+		
+		System.out.println("Welcome to the Multiplayer Pacman Game");
+		System.out.println("Before we get started. Could you please tell us your name:");
+		String userName = player.nextLine();
+
+		String serverOption = null;
+
+		System.out.println("Welcome " + userName + ". Would you like to Host or Join a Server?");
+		while (serverOption == null) {
+			serverOption = getServerOption(player.nextLine());
+		}
+		
+		String ipAddress = null;
+		
+		while (serverOption == "join" && ipAddress == null) {
+			System.out.println("Please input the host's IP address");
+			ipAddress = validateIPaddress(address.nextLine());
+		}
+		
+		if (serverOption == "host") {
+			String serverIp;
+			try {
+				Server server = new Server();
+				serverIp = server.getHostAddress();
+				server.registerOutput(new SimpleOutPut(null, serverIp));
+				System.out.println("The host address is " + serverIp);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			
+			
+			
+		}
+		
+		if (serverOption == "join") {
+			try {
+				Client client = new Client(ipAddress);
+				client.registerOutput(new TemplateFrame(client,client.getPlayerNum().toString()));				
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+			
+	}
+
+	
+	
+	static String getServerOption(String input) {
+		if (input.toLowerCase().equals("join")) {
+			return "join";
+		} else if (input.toLowerCase().equals("host")) {
+			return "host";
+		} else {
+			System.out.println("Please try again. Would you like to Join or Host a server?");
+			return null;
+		}
+
+	}
+
+	static String validateIPaddress(String input) {
+		String regex = "\\b((25[0–5]|2[0–4]\\d|[01]?\\d\\d?)(\\.)){3}(25[0–5]|2[0–4]\\d|[01]?\\d\\d?)\\b";
+		if (Pattern.matches(regex, input)) {
+			System.out.println("True");
+			return input;
+		}
+		else {
+			return null;
+		}
+		
+
+	}
+
+}
+
+
