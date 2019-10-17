c7081aace2a0e55a053b94887ad8e3f5d538153b
s3722271 <37519572+s3722271@users.noreply.github.com>
2019-10-17 18:11:44 +1100

    Add files via upload
    JUnit Testing for launcher class

---
+++ multiplayerPacMan/src/game/launcherTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class launcherTest {
+
+	//@Test
+	//void goodIP() {
+		//assertNotEquals(launch.validateIPaddress("192.168.1.1"),null);
+	//}
+
+	@Test
+	void badIP() {
+		assertEquals(launch.validateIPaddress("a.a.a.a"),null);	
+	}
+	
+	@Test
+	void serverOption() {
+		assertEquals(launch.getServerOption("aaaa"),null);	
+	}
+	
+	@Test
+	void serverOptionJoin() {
+		assertEquals(launch.getServerOption("join"),"join");	
+	}
+
+	@Test
+	void serverOptionHost() {
+		assertNotEquals(launch.getServerOption("Host"),null);	
+	}
+
+
+
+
+
+}
